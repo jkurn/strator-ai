70c42e1f54563e4e0acbb2023e72b77f97410115
Jonathan Kurniawan <jonathan.leslie17@gmail.com>
2026-03-16 20:49:10 +1100

    Redesign: editorial fintech aesthetic — DM Sans, bento grid, vertical timeline, noise texture, border beam

---
+++ _next/static/chunks/app/layout-0b3a646a0ebb0c63.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4343:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9469,23))},9469:()=>{}},e=>{e.O(0,[506,814,743,358],()=>e(e.s=4343)),_N_E=e.O()}]);
